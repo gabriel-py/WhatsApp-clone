70fb51052848867711249d343c79885ba4ca90fa
Gabriel Gonçalves <gabrielgoncalves310503@gmail.com>
2021-05-17 16:54:45 -0300

    first commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBo9hzsQGRWae4NjBwEeaMjbrPMKb-VqnA",
+    authDomain: "whatsapp-clone-81613.firebaseapp.com",
+    projectId: "whatsapp-clone-81613",
+    storageBucket: "whatsapp-clone-81613.appspot.com",
+    messagingSenderId: "946548461274",
+    appId: "1:946548461274:web:343f263d7e43171a68dfe3",
+    measurementId: "G-EBSX3SYH3H"
+};
